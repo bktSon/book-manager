05f5f159f327095adc5cbb20add24eb6a731f736
achiliku <kimsonbui303@gmail.com>
2018-06-22 14:47:56 +0700

    update app

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import UserController from './users.controller';
 export const userRouter = express.Router();
-
-userRouter.route('/')
-    .get(UserController.findAll)
-    .post(UserController.create)
-;
-
-userRouter.route('/:id')
-    .put(middleWare, UserController.update)
-    .delete(middleWare, UserController.delete)
-;
-
-
+//
+// userRouter.route('/')
+//     .get(UserController.findAll)
+//     .post(UserController.create)
+// ;
+//
+// userRouter.route('/:id')
+//     .put(middleWare, UserController.update)
+//     .delete(middleWare, UserController.delete)
+// ;
+//
+//
